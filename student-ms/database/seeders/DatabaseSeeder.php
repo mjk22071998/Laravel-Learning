afc407b92454fc7aa0482fd6b421ae12dc61ae22
mjk22071998 <mjk22071998@gmail.com>
2024-12-13 16:29:07 +0500

    Modelss migrations and seeder created

--- student-ms/database/seeders/DatabaseSeeder.php
+++ student-ms/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Administrator',
+            'email' => 'admin@example.com',
+            'password' => bcrypt(env('ADMIN_PASS')),
+            'role' => 'admin'
         ]);
     }
 }
