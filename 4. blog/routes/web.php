c6f3af8f817dd8da0c9b09740d297a82e9e38512
mjk22071998 <mjk22071998@gmail.com>
2025-01-15 15:14:29 +0500

    Database migrtion complete

--- 4. blog/routes/web.php
+++ 4. blog/routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PagesController::class, 'getIndex'])->name('home');
 Route::get('/about', [PagesController::class, 'getAbout'])->name('about');
 Route::get('/contact', [PagesController::class, 'getContact'])->name('contact');
+Route::resource('post', PostController::class);
